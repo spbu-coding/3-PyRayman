744a84b3a0f5f65576bca9c2dc692aad9050e81f
PyRayman <rizvan.mallaev@mail.ru>
2020-10-30 13:55:46 +0300

    compound fix Commit

--- src/main.c
+++ src/main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+// Func for float
 
-float distance(float *p1, float *p2)
+float distance_f(float *p1, float *p2)
 {
     return sqrt(pow((p1[0] - p2[0]), 2) + pow((p1[1] - p2[1]), 2));
 }
 
 
-float norm(float *mat)
+float norm_f(float *mat)
 {
     return mat[0] * mat[3] - mat[1] * mat[2];
 }
 
 
 
-void inverse (float *mat, float *inv, float norm)
+void inverse_f (float *mat, float *inv, float norm)
 {
     inv[0] = mat[3]/norm;
     inv[1] = -mat[1]/norm;
@@ -24,7 +25,35 @@ void inverse (float *mat, float *inv, float norm)
 }
 
 
-void matmultiplication (float *mat1, float *mat2, float *result)
+void matmultiplication_f (float *mat1, float *mat2, float *result)
+{
+    result[0] = mat1[0] * mat2[0] + mat1[1] * mat2[1];
+    result[1] = mat1[2] * mat2[0] + mat1[3] * mat2[1];
+}
+
+
+// Func for double
+
+double distance_d(double *p1, double *p2)
+{
+    return sqrt(pow((p1[0] - p2[0]), 2) + pow((p1[1] - p2[1]), 2));
+}
+
+
+double norm_d(double *mat)
+{
+    return mat[0] * mat[3] - mat[1] * mat[2];
+}
+
+void inverse_d (double *mat, double *inv, double norm)
+{
+    inv[0] = mat[3]/norm;
+    inv[1] = -mat[1]/norm;
+    inv[2] = -mat[2]/norm;
+    inv[3] = mat[0]/norm;
+}
+
+void matmultiplication_d (double *mat1, double *mat2, double *result)
 {
     result[0] = mat1[0] * mat2[0] + mat1[1] * mat2[1];
     result[1] = mat1[2] * mat2[0] + mat1[3] * mat2[1];
@@ -32,36 +61,71 @@ void matmultiplication (float *mat1, float *mat2, float *result)
 
 int main()
 {
-    float d = 0.00001;
-    float ds, dprev, dsprev;
+    // variable for float
+    float d_f = 0.00001;
+    float ds_f, dprev_f, dsprev_f;
 
-    float A[4] = {1.0, 1.0, 1.0, 1.00001};
-    float b[2] = {2.0, 2.00001 + d};
+    float A_f[4] = {1.0, 1.0, 1.0, 1.00001};
+    float b_f[2] = {2.0, 2.00001 + d_f};
 
-    float invA[4];
-    inverse(A, invA, norm(A));
+    float invA_f[4];
+    inverse_f(A_f, invA_f, norm_f(A));
 
-    float result1[2] = {1.0, 1.0}; // Координата  b = {2.0, 2.00001}
+    float result1_f[2] = {1.0, 1.0}; // Координата  b = {2.0, 2.00001}
 
-    float result2[2]; // Координата  b = {2.0, 2.00001 + d}
-    matmultiplication(invA, b, result2);
+    float result2_f[2]; // Координата  b = {2.0, 2.00001 + d}
+    matmultiplication_f(invA_f, b_f, result2_f);
 
-    ds = distance(result1, result2);
+    ds_f = distance_f(result1_f, result2_f);
 
-    while (ds > 0.000001)
+    // variable for double
+    double d_db = 0.00001;
+    double ds_db, dprev_db, dsprev_db;
+
+    double A_db[4] = {1.0, 1.0, 1.0, 1.00001};
+    double b_db[2] = {2.0, 2.00001 + d_db};
+
+    double invA_db[4];
+    inverse_f(A_db, invA_db, norm_db(A));
+
+    double result1_db[2] = {1.0, 1.0}; // Координата  b = {2.0, 2.00001}
+
+    double result2_db[2]; // Координата  b = {2.0, 2.00001 + d}
+    matmultiplication_db(invA_db, b_db, result2_db);
+
+    ds_db = distance_db(result1_db, result2_db);
+
+
+    while (ds_db > 0.000001)
     {
-        dprev = d;
-        dsprev = ds;
+        dprev_db = d_db;
+        dsprev_db = ds_db;
 
-        d /= 2;
-        b[1] = 2.00001 + d;
+        d_db /= 2;
+        b_db[1] = 2.00001 + d_db;
 
-        matmultiplication(invA, b, result2);
+        matmultiplication_db(invA_db, b_db, result2_db);
 
-        ds = distance(result1, result2);
+        ds_db = distance(result1_db, result2_db);
     }
 
-    printf("last D =\t%.20f\nDistance =\t\t%.7f\n\nstopping D =\t%.20f\nDistance =\t\t%.7f\n", dprev, dsprev, d, ds);
+    while (ds_f > 0.000001)
+    {
+        dprev_f = d_f;
+        dsprev_f = ds_f;
+
+        d_f /= 2;
+        b_f[1] = 2.00001 + d_f;
+
+        matmultiplication_f(invA_f, b_f, result2_f);
+
+        ds_f = distance(result1_f, result2_f);
+    }
+    printf("Result for Double: \n");
+    printf("last D =\t%.20f\nDistance =\t\t%.7f\n\nstopping D =\t%.20f\nDistance =\t\t%.7f\n", dprev_db, dsprev_db, d_db, ds_db);
+
+    printf("Result for Float: \n");
+    printf("last D =\t%.20f\nDistance =\t\t%.7f\n\nstopping D =\t%.20f\nDistance =\t\t%.7f\n", dprev_f, dsprev_f, d_f, ds_f);
 
     return 0;
     
